5c0b967a521f5a1eef242155a728dba1918be703
Eldrige <peldrige8@gmail.com>
2024-05-06 13:59:54 +0200

    initial commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports.SITE = {
+  name: 'TailNext',
+
+  origin: 'https://tailnext.vercel.app',
+  basePathname: '/',
+  trailingSlash: false,
+
+  title: 'PDCA — Purpose Driven Counselling Center',
+  description: 'Passion, Determination and Success.',
+};
